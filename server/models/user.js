0ca12fbd8e86ed0c5d088404de7279fa1b8c486a
John Malapit <johncmalapit@gmail.com>
2022-07-21 15:52:46 -0400

    remove passwordHash from return object

--- server/models/user.js
+++ server/models/user.js
@@ -16,6 +16,15 @@ const userSchema = new mongoose.Schema({
   currencies: Array,
 });
 
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  },
+});
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
