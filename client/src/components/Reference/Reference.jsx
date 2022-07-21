b69a68d0a713a0b862b93602f4addb40f4e46468
Aryse Tansy <medictansy@gmail.com>
2022-07-21 18:58:46 -0400

    Client: interface, reference list

---
+++ client/src/components/Reference/Reference.jsx
@@ -0,0 +1,35 @@
+import classes from "./Reference.module.css";
+import currencyList from "../../data/currencyList";
+import Portal from "../Portal/Portal";
+import { useContext } from "react";
+import { uiCtx } from "../../store/ui-ctx";
+
+const Reference = () => {
+  const uiCtxMgr = useContext(uiCtx);
+
+  const closeReferenceHandler = () => {
+    uiCtxMgr.setShowModal(false);
+  };
+
+  return (
+    <Portal>
+      <article className={classes.article}>
+        <ul className={classes.ul}>
+          {currencyList.map((obj, index) => {
+            return (
+              <li className={classes.li}>
+                <span>{Object.values(obj)[0]}</span>{" "}
+                <span>{Object.keys(obj)[0]}</span>
+              </li>
+            );
+          })}
+        </ul>
+        <button onClick={closeReferenceHandler} className={classes.submit}>
+          Close
+        </button>
+      </article>
+    </Portal>
+  );
+};
+
+export default Reference;
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import AuthProvider from "./store/auth-ctx";
+import UiProvider from "./store/ui-ctx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <UiProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </UiProvider>
   </React.StrictMode>
 );
