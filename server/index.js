16762299473104aef360848700a5460d1f08c9ec
John Malapit <johncmalapit@gmail.com>
2022-07-23 13:33:50 -0400

    implement backend PUT request for deleting currencies from user list

--- server/index.js
+++ server/index.js
@@ -55,9 +55,27 @@ app.post("/api/saveCurrencies", async (req, res) => {
     user.save();
     res.json(user);
   });
+});
 
-  // const user = await User.findOne(mongoose.Types.ObjectId(`${userID}`));
-  // console.log("user", user);
+// PUT: REMOVE ENTRY FROM FAVE LIST
+app.put("/api/deleteCurrency", async (req, res) => {
+  let { id, original, convertTo } = req.body;
+  const objToDelete = { original: original, convertTo: convertTo };
+
+  User.find({ _id: id }, (err, doc) => {
+    if (err) {
+      console.log(err);
+    }
+
+    const user = doc[0];
+    user.currencies = [...user.currencies].filter(
+      (cur) =>
+        Object.entries(cur).toString() !==
+        Object.entries(objToDelete).toString()
+    );
+    user.save();
+  });
+  res.json(objToDelete);
 });
 
 // UNHANDLED
